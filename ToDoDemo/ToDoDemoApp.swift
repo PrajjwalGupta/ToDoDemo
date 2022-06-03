f03ecf474e149d69635ddbe960bace4db72cd526
Prajjwal Gupta <31537066+PrajjwalGupta@users.noreply.github.com>
2022-06-03 11:16:50 +0530

    Initial Commit

---
+++ ToDoDemo/ToDoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDemoApp.swift
+//  ToDoDemo
+//
+//  Created by Prajjawal Gupta on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
